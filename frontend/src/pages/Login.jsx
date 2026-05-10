1c2e9c781a0d40ce23507ac5a473612832afb66d
Rahulsahu7389 <rahulsahuppd@gmail.com>
2026-05-10 21:56:38 +0530

    changed workflow

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -67,10 +67,10 @@ export default function Login() {
 
         <form onSubmit={handleAuth} className="space-y-4">
           <div className="relative">
-            <Mail className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" />
+            <Mail className="absolute left-4 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" />
             <input
               type="email"
-              className="input-soft-border-focus pl-10"
+              className="input-soft-border-focus pl-12"
               placeholder="Email address"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
@@ -78,10 +78,10 @@ export default function Login() {
             />
           </div>
           <div className="relative">
-            <Lock className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" />
+            <Lock className="absolute left-4 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" />
             <input
               type="password"
-              className="input-soft-border-focus pl-10"
+              className="input-soft-border-focus pl-12"
               placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
@@ -98,12 +98,12 @@ export default function Login() {
               <Loader2 className="w-5 h-5 animate-spin" />
             ) : isLogin ? (
               <>
-                <LogIn className="w-5 h-5" />
+                <LogIn className="w-5 h-5 mr-1" />
                 Sign In
               </>
             ) : (
               <>
-                <UserPlus className="w-5 h-5" />
+                <UserPlus className="w-5 h-5 mr-1" />
                 Sign Up
               </>
             )}
@@ -123,7 +123,7 @@ export default function Login() {
           onClick={handleGoogleSignIn}
           className="btn-delicate-interaction w-full border border-gray-200 bg-white hover:bg-gray-50 text-gray-700 py-2.5 mb-6"
         >
-          <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24">
+          <svg className="w-5 h-5 mr-3" viewBox="0 0 24 24">
             <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
             <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
             <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/>
